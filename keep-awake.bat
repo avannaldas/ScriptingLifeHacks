d454c26613e73ac11e1b420ab6a2d12683bf5dda
Abhijit Annaldas <avannaldas@users.noreply.github.com>
2017-06-29 02:20:17 +0530

    Update keep-awake.bat

--- keep-awake.bat
+++ keep-awake.bat
@@ -1,7 +1,18 @@
 echo off
+
+rem Make a tiny window enough to show a single line message
 mode con: cols=50 lines=2
 cls
+
+rem Print one time message
 echo Just keeping the computer awake
+
 :loop
-ping 1.1.1.1 -n 1 -w 60000 > nul
+rem Ping once, throw away standard output
+ping 1.1.1.1 -n 1 > nul
+
+rem Wait for 5 minutes, throw away standard output
+timeout 300 > nul
+
+rem Repeat
 goto loop
