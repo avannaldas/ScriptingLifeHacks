d597d0c98e7b8216e496108b2c6182fee340ac83
Abhijit Annaldas <avannaldas@users.noreply.github.com>
2017-07-20 01:45:20 +0530

    Update beep-every-min.bat

--- beep-every-min.bat
+++ beep-every-min.bat
@@ -1,5 +1,19 @@
- :loop
- python -c "print('\7')"
- timeout /t 60
- goto loop
- 
+echo off
+
+rem Make a tiny window enough to show a single line message
+mode con: cols=60 lines=2
+cls
+
+:loop
+
+rem Print a message
+echo Beeping every min to keep bluetooth headset connected
+
+rem Wait for a minute, throw away standard output
+timeout /t 60 > nul
+
+rem Beep using python
+python -c "print('\7')"
+
+rem Repeat
+goto loop
